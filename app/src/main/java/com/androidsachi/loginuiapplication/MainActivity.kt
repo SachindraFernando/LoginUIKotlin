2d9c48f9f7a50620edda1fca7a6e7295fd114750
Sachindra Fernando <37910436+SachindraFernando@users.noreply.github.com>
2019-10-21 23:08:01 +0530

    Update MainActivity.kt

--- app/src/main/java/com/androidsachi/loginuiapplication/MainActivity.kt
+++ app/src/main/java/com/androidsachi/loginuiapplication/MainActivity.kt
@@ -3,15 +3,24 @@ package com.androidsachi.loginuiapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mLoginBtn2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(applicationContext , LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        mLoginBtn2 = findViewById(R.id.button3)
+
+        mLoginBtn2.setOnClickListener{
+            val intent = Intent(applicationContext , LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
+
